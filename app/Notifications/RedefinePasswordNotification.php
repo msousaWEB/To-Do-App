643b4d9094e6d0ac683eda1a8538bc2122402801
msousa <msousa.dev@outlook.com>
2022-07-24 11:20:53 -0300

    More customizations for email notification

---
+++ app/Notifications/RedefinePasswordNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class RedefinePasswordNotification extends Notification
+{
+    use Queueable;
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = 'http://localhost:8000/password/reset/'.$this->token;
+        $min = config('auth.passwords.'.config('auth.defaults.passwords').'.expire');
+        return (new MailMessage)
+            ->subject(Lang::get('Recuperação de senha'))
+            ->line(Lang::get('Recupere a sua senha através do botão de resetar.'))
+            ->action(Lang::get('Resetar senha'), $url)
+            ->line(Lang::get('Este link expira em :count minutos.', ['count' => $min]))
+            ->line(Lang::get('Por favor, ignore este email caso não tenha solicitado o reset de senha.'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'To-Do App')
-<img src="http://localhost:8000/img/logo.png" class="logo" alt="Laravel Logo">
+<img src="http://localhost:8000/img/logo.png" class="logo" alt="To-Do App">
 @else
 {{ $slot }}
 @endif
