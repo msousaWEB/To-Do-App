5459d871e5d7ae49ed560e2c76a5ccdf5654784e
msousa <msousa.dev@outlook.com>
2022-07-25 21:34:04 -0300

    Bugfix export;

--- app/Exports/TaskExport.php
+++ app/Exports/TaskExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Task;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TaskExport implements FromCollection
+class TaskExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +15,17 @@ class TaskExport implements FromCollection
     {
         // return Task::all();
         return auth()->user()->tasks()->get();
+    }
+
+    public function headings():array
+    {
+        return [
+                'ID', 
+                'ID Usuário', 
+                'Tarefa', 
+                'Data estimada para conclusão',
+                'Data criação',
+                'Data atualização'
+        ];
     }
 }
